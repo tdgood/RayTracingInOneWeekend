3dfc2eeabe534b1c9d4222057bedef287d07c9a2
Trevor Good <tdgood98@gmail.com>
2020-12-07 16:41:08 -0500

    Add random_double utility functions

--- rtweekend.h
+++ rtweekend.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
 // Usings
 using std::shared_ptr;
@@ -17,6 +18,14 @@ const double pi = 3.1415926535897932385;
 // Utility Functions
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    return min + (max - min) * random_double();
 }
 
 // Common Headers
